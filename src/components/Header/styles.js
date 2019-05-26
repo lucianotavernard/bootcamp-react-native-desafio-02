dda64a8aceb69bcff2c5543f28b1ffbc5092a7a2
luciano tavernard <ltms00@hotmail.com>
2019-05-26 18:43:22 -0300

    Criado o componente Header das páginas

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 45px;
+  margin-bottom: 20px;
+  background: #fff;
+`;
+
+export const Title = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
